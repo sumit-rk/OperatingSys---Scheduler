7a0179f40834b1215e782b8075cf191e3fe20dee
Sumit Kumar Rajput <sumit26may@gmail.com>
2020-03-28 11:22:33 +0530

    fcfsv2.c
    Updated program to calculate average Turnaround Time and Waiting Time and also factilitating the use of SJF to the same data.

--- fcfs.c
+++ fcfs.c
@@ -1,53 +1,52 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-struct Processes
-{
-  int p_id;
-  int burst_time;
-};
+int procs[10][6];
 
-int wait_time(int n,struct Processes p[],int *waiting_time)
+int wait_time(int n,int procs[][6])
 { int i;
-  waiting_time[0] = 0;
+  procs[0][4] = 0;
   for(i=1; i<n; i++)
   {
-    waiting_time[i] = p[i-1].burst_time + waiting_time[i-1];
+    procs[i][4] = procs[i-1][2] + procs[i-1][4];
   }
 }
 
-int turnaround_time(int n,struct Processes p[],int waiting_time[],int *ta_time)
+int turnaround_time(int n,int procs[][6])
 { int k;
   for (k=0; k<n; k++)
   {
-    ta_time[k] = p[k].burst_time + waiting_time[k];
+    procs[k][5] = procs[k][2] + procs[k][4];
   }
 }
 
-int avg_time(int n,struct Processes p[])
+int avg_time(int n,int procs[][6])
 {
-  int waiting_time[n];
-  int ta_time[n];
-  int total_wtime =0;
-  int total_ta_time =0;
+  double total_wtime =0.0;
+  double total_ta_time =0.0;
   int j;
 
-  wait_time(n,p,waiting_time);
-  turnaround_time(n,p,waiting_time,ta_time);
+  wait_time(n,procs);
+  turnaround_time(n,procs);
 
   for(j=0; j<n; j++)
   {
-    total_wtime = total_wtime + waiting_time[j];
-    total_ta_time = total_ta_time + ta_time[j];
-    printf("\n Process number          :%d",j+1);
-    printf("\n Process burst time      :%d",p[j].burst_time);
-    printf("\n Process waiting time    :%d",waiting_time[j]);
-    printf("\n Process turnaround time :%d",ta_time[j]);
-    printf("\n");
+    total_wtime = total_wtime + procs[j][4];
+    total_ta_time = total_ta_time + procs[j][5];
   }
 
-  printf("\n Average Waiting time     : %d",total_wtime/n);
-  printf("\n Average Turnaround time  : %d",total_ta_time/n);
+  printf("\nProcess ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time");
+
+  for(j=0; j<n; j++)
+  {
+    printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",procs[j][0],procs[j][1],procs[j][2],procs[j][4],procs[j][5] );
+  }
+  printf("\n");
+  printf("\nTotal Waiting time     : %lf",total_wtime);
+  printf("\nTotal Turnaround time  : %lf",total_ta_time);
+  printf("\n");
+  printf("\nAverage Waiting time     : %lf",total_wtime/n);
+  printf("\nAverage Turnaround time  : %lf",total_ta_time/n);
 }
 
 int main()
@@ -56,15 +55,15 @@ int main()
   printf("\n Enter the total number of processes : ");
   scanf("%d",&n);
 
-  struct Processes procs[n];
   printf("\n Enter the details of the processes : ");
   for(i=0; i<n; i++)
   {
-     printf("\n Details of P-%d",i+1);
-     printf("\n Arrival time of the P-%d",i+1);
+     printf("\n Details of P-%d....  ",i+1);
+     printf("\n Arrival time of the P-%d.....  ",i+1);
      scanf("%d",&ari_time);
-     procs[i].p_id = i+1;
-     procs[i].burst_time = 2*ari_time;
+     procs[i][0] = i;
+     procs[i][1] = ari_time;
+     procs[i][2] = 2*ari_time;
   }
 
   avg_time(n,procs);
